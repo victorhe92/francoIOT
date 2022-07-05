613d855a96372de20e1db5ea765e97bccf39b91f
PabloCarreraJs <pcarrera.mkt@gmail.com>
2022-07-05 05:55:24 -0400

    update

--- Utils.py
+++ Utils.py
@@ -15,3 +15,32 @@ def save_in_file(db_file, data):
 def get_date():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+def check_user(name, password):
+    with open(control_data, "r") as f:
+        data = f.readlines()
+    for line in data:
+        if name in line and password in line:
+            return True 
+    return False
+
+def get_plot_data(db_file):
+    with open(db_file, "r") as f:
+        data = f.readlines()
+    return data
+
+def register_lote(db_file, lote):
+    with open(db_file, "a") as f:
+        f.writelines(lote+"\n")
+
+def get_data(db_file):
+    with open(db_file, "r") as f:
+        data = f.readlines()
+    return data
+
+def delete_data(db_file, data):
+    with open(db_file, "w") as f:
+        for line in data:
+            if line != data:
+                f.writelines(line)
+
+
