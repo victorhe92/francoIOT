378f8bcf0dc18aa25a9f5dd641ad2f462ce6dac8
PabloCarreraJs <pcarrera.mkt@gmail.com>
2022-07-15 01:34:36 -0400

    reset

--- inicio.py
+++ inicio.py
@@ -1,7 +1,7 @@
 import tkinter
 import customtkinter
 from mainFrame import mainFrame
-import matplotlib.pyplot as plt
+
 import datetime
 
 
@@ -17,12 +17,41 @@ class inicio():
     
     def __init__(self,master):
         #Create a general vision frame
+        self.frame = customtkinter.CTkFrame(master=master)
+
         self.frame.grid(row=0, column=1,sticky="nsew",pady=10,padx=10)
-        self.frame = customtkinter.CTkFrame(master=self.frame)
-
         
-   
+        # ============ frame_sensors ============
+        
+        # configure grid layout (3x2)
+        
+        self.frame.columnconfigure(0,weight=1)
+        self.frame.columnconfigure(1,weight=1)
+        self.frame.rowconfigure(0,weight=1)
+        self.frame.rowconfigure(1,weight=1)
+        self.frame.rowconfigure(2,weight=1)
+        
+        self.label_1 = customtkinter.CTkLabel(master=self.frame,
+                                              text="Control de microtuneles",
+                                              text_font=("Roboto Medium", -25))  # font name and size in px
+        self.label_1.grid(row=0, column=0, pady=10, padx=10, columnspan=2)
+        self.label_1 = customtkinter.CTkLabel(master=self.frame,
+                                              text=self.tiempo,
+                                              text_font=("Roboto Medium", -12))  # font name and size in px
+        self.label_1.grid(row=0, column=0, pady=10, padx=10, columnspan=2,sticky="e")
 
+        self.frame_station1 = customtkinter.CTkFrame(master=self.frame)
+        #self.frame_station1.grid_propagate(0)
+        self.frame_station1.grid(row=1, column=0,pady=20, padx=20, sticky="nsew")
+
+        self.frame_station2 = customtkinter.CTkFrame(master=self.frame)
+        self.frame_station2.grid(row=1, column=1, pady=20, padx=20, sticky="nsew")
+
+        self.frame_station3 = customtkinter.CTkFrame(master=self.frame)
+        self.frame_station3.grid(row=2, column=0, pady=20, padx=20, sticky="nsew")
+
+        self.frame_station4 = customtkinter.CTkFrame(master=self.frame)
+        self.frame_station4.grid(row=2, column=1, pady=20, padx=20, sticky="nsew")
 
         #==============INFORMATION OF EACH STATION======================
 
@@ -169,6 +198,6 @@ class inicio():
 if __name__=="__main__":
     app = mainFrame()
     windows_start = inicio(app)
-    #app.show_frame(windows_start)
+    app.show_frame(windows_start)
     #app.hide_frame()
     app.mainloop()
